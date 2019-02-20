03817cadbfa075c86bcc332fa97dcf5cd1dbf17b
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2019-02-20 07:26:56 +0100

    Fjerner kommentarer i extensions

--- source/conf.py
+++ source/conf.py
@@ -38,14 +38,7 @@ release = u'1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx_tabs.tabs'
-    # 'sphinx.ext.autodoc',
-    # 'sphinx.ext.todo',
-    # 'sphinx.ext.ifconfig',
-    # 'sphinx.ext.viewcode',
-    # 'sphinx.ext.githubpages',
-]
+extensions = [ 'sphinx_tabs.tabs' ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
